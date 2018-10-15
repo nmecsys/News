28d12f28cf9f6c1412daf98435fbd11a285a5eed
Jonatha Azevedo <johnmasterchip@gmail.com>
2018-10-15 12:21:12 -0300

    Atualizando script + incriptação de credenciais

--- src/index_noticias_elasticsearch.py
+++ src/index_noticias_elasticsearch.py
@@ -5,34 +5,84 @@ Created on Fri Mar  9 10:54:54 2018
 @author: jonatha.costa
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 14 16:02:43 2018
+
+@author: jonatha.costa
+"""
+
+
 from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
+from pymongo import MongoClient
+from pandas.io.json import json_normalize
 import pandas as pd 
+import numpy as np
+import requests
 import json
-
-df = pd.read_csv("mediacloud_export.csv",sep=",")
-
-df["id"] = [x+1 for x in range(len(df["id"]))]
-tmp = df.to_json(orient = "records")
-
-index_name = 'estadao'
-type_name = 'noticias'
+import time
+import os
+from cryptography.fernet import Fernet
 
 
-es = Elasticsearch('localhost')
+file = open('key_elasticsearch.key','rb')
+key = file.read()
+file.close()
 
-actions = [
-     {
-     '_index' : index_name,
-     '_type' : type_name,
-     '_id': df['id'],
-     '_website' : df['website'],
-     '_url' : df['url'],
-     '_publication_date' : df['publication_date'],
-     '_title': df['title'],
-     '_text': df['text'],
-     }
-for df in json.loads(tmp)
-]
 
-bulk(es, actions)
+f = Fernet(key)
+credenciais = pd.read_table('crendialsENCRIPT.txt').columns[0].encode()
+
+credenciais = f.decrypt(credenciais).decode()
+es = Elasticsearch(credenciais)
+
+
+
+df = pd.DataFrame()
+
+
+def pesquisa(termo = None):
+
+    page = es.search(
+      index = 'extra',
+      doc_type = 'noticias',
+      scroll = '2m',
+      size = 10000,
+      request_timeout=30,
+      body = {
+            'query': {
+                'match' : {
+                      '_text':  str(termo)
+                        }
+                }
+        })
+    
+    return(print("Foram encontrados %d noticias que se encaixam com o termo pesquisado!" % page['hits']['total'] ))
+
+pesquisa(termo = 'Dilma')
+
+
+
+
+sid = page['_scroll_id']
+scroll_size = page['hits']['total']
+  
+
+  # Start scrolling
+while (scroll_size > 0):
+   print("Scrolling...")
+   page = es.scroll(scroll_id = sid, scroll = '2m',request_timeout=30)
+   # Update the scroll ID
+   sid = page['_scroll_id']
+   # Get the number of results that we returned in the last scroll
+   scroll_size = len(page['hits']['hits'])
+   kaux =page['hits']['hits']
+   df_aux = json_normalize(page['hits']['hits'])
+   df =df.append(df_aux,ignore_index = True)
+   print("scroll size: " + str(scroll_size))
+   # Do something with the obtained page
+   
+   df = df.sort_index(ascending=False)
+   
+   
